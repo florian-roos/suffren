7b836e1179805174df3b452ea58445013d26811c
florian-roos <florian.roos@telecom-paris.fr>
2026-03-01 14:29:59 +0100

    feat: implement Suffren public API (New, Increment, Value, Stop)

---
+++ pkg/suffren/suffren.go
@@ -0,0 +1,63 @@
+package suffren
+
+import (
+	"suffren/internal/crdt"
+	latticeagreement "suffren/internal/lattice-agreement"
+	"suffren/internal/node"
+	"suffren/internal/p2p"
+	"sync"
+)
+
+type Suffren struct {
+	mu           sync.RWMutex
+	node         *node.Node
+	localCounter *crdt.GCounter
+	la           *latticeagreement.LatticeAgreement
+}
+
+func NewSuffren(port string, peers map[crdt.NodeId]string) (*Suffren, error) {
+	nodeIds := make([]crdt.NodeId, len(peers))
+	for nodeId := range peers {
+		nodeIds = append(nodeIds, nodeId)
+	}
+
+	suffren := &Suffren{
+		mu:           sync.RWMutex{},
+		localCounter: crdt.NewGCounter(nodeIds),
+	}
+
+	network := p2p.NewNetwork(port, peers)
+
+	suffren.la = latticeagreement.NewLatticeAgreement(crdt.NodeId(port), peers, network, suffren.localCounter.Bottom(), func(learnedValue crdt.Lattice) {
+		suffren.mu.Lock()
+		defer suffren.mu.Unlock()
+		suffren.localCounter.MergeInPlace(learnedValue)
+	})
+
+	messageHandler := node.NewLAMessageHandler(suffren.la)
+	suffren.node = node.NewNode(port, peers, network, messageHandler)
+
+	suffren.node.Start()
+
+	return suffren, nil
+}
+
+// Increment increments the local counter and proposes the new value to the cluster.
+func (s *Suffren) Increment() {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.localCounter.Increment(s.node.Id)
+	s.la.Proposer.Propose(s.localCounter)
+}
+
+// Return the current value of the counter. Note that this function is wait-free and the value may be stale if there are ongoing proposals in the cluster.
+func (s *Suffren) Value() uint64 {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.localCounter.Value()
+}
+
+// Stop the node and its network service gracefully.
+func (s *Suffren) Stop() {
+	s.node.Stop()
+}
