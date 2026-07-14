3aeb8ac17b8ae38347d1d2586434f29f87dc2f6c
florian-roos <florian.roos@telecom-paris.fr>
2026-07-14 14:35:48 +0200

    feat: introduce bufio buffering to connection encoder and decoder for improved performance

--- internal/p2p/connection.go
+++ internal/p2p/connection.go
@@ -1,30 +1,38 @@
 package p2p
 
 import (
+	"bufio"
 	"encoding/gob"
 	"net"
+
 	"github.com/florian-roos/suffren/internal/protocol"
 )
 
 type Connection struct {
 	conn    net.Conn
+	writer  *bufio.Writer
 	encoder *gob.Encoder
 	decoder *gob.Decoder
 }
 
 func NewConnection(conn net.Conn) *Connection {
-
+	writer := bufio.NewWriter(conn)
+	reader := bufio.NewReader(conn)
 	connection := Connection{
 		conn:    conn,
-		encoder: gob.NewEncoder(conn),
-		decoder: gob.NewDecoder(conn),
+		writer:  writer,
+		encoder: gob.NewEncoder(writer),
+		decoder: gob.NewDecoder(reader),
 	}
 	return &connection
 }
 
 func (connection Connection) Send(message protocol.Message) error {
-
-	return connection.encoder.Encode(message)
+	err := connection.encoder.Encode(message)
+	if err == nil {
+		err = connection.writer.Flush()
+	}
+	return err
 }
 
 func (connection Connection) Receive() (protocol.Message, error) {
